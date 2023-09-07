1cc8f332a3e73a73b4b2b57719e7bc6cd482237e
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2023-09-07 17:15:17 -0400

    OK, still get deviations, but forcing the neighbor list to be recomputed did have a significant impact

---
+++ lammps_related/lammps_jl_tests/md_loops/debugging_single_vs_batch/attempt2/longer/lammps_ace_single_step.jl
@@ -0,0 +1,34 @@
+using LAMMPS
+
+LAMMPS.locate()
+lmp = LMP(["-screen","none"])
+
+command(lmp, "units          metal")
+command(lmp, "boundary       p p p")
+command(lmp, "atom_style     atomic")
+command(lmp, "neigh_modify   delay 0 every 1 check no")
+
+command(lmp, "read_data     tetrag_hfo2_sample_DATA")
+
+command(lmp, "pair_style     pace")
+command(lmp, "pair_coeff     * * Siviraman_GAP_HfO2_N3_rcut5_maxdeg10_regQR.yace Hf O")
+
+command(lmp, "variable T        equal  2000")
+command(lmp, "variable Tdamp    equal  0.1")
+command(lmp, "variable Tseed    equal  12280329")
+command(lmp, "variable dumpf    equal  1")
+
+command(lmp, "velocity     all create \$T \${Tseed} mom yes rot yes dist gaussian")
+
+command(lmp, "thermo       1")
+command(lmp, "thermo_style custom step temp pe ke etotal press")
+
+command(lmp, "dump           run_forces all custom \${dumpf} dump_single.custom id type x y z fx fy fz vx vy vz")
+command(lmp, """dump_modify    run_forces sort id format line "%4d %1d %32.27f %32.27f %32.27f %32.27f %32.27f %32.27f %32.27f %32.27f %32.27f" """)
+
+
+command(lmp, "fix          nvt all nvt temp \$T \$T \${Tdamp}")
+
+for i in 1:50
+    command(lmp, "run 1")
+end

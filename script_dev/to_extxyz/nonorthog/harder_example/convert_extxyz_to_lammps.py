b5893b50e4d4d5dcc3b60512e270928771324d0c
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2023-09-21 15:35:26 -0400

    testing nonorthog xyz to lammps conversion for tricky example

---
+++ script_dev/to_extxyz/nonorthog/harder_example/convert_extxyz_to_lammps.py
@@ -0,0 +1,8 @@
+import ase.io
+import sys 
+
+input_fname = str(sys.argv[1])
+output_fname = str(sys.argv[2])
+
+atoms = ase.io.read(input_fname)
+ase.io.write(output_fname,atoms,format="lammps-data")

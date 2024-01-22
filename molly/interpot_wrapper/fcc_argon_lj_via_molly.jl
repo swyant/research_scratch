67d91909f1e072d5736af339bbaaf14c865453a8
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2024-01-22 17:51:08 -0500

    working interface with unit conversion for LJ

--- molly/interpot_wrapper/fcc_argon_lj_via_molly.jl
+++ molly/interpot_wrapper/fcc_argon_lj_via_molly.jl
@@ -1,38 +1,6 @@
-using Molly
-using InteratomicPotentials
-using AtomsIO 
-using Unitful
-using AtomsBase
- 
-# Wrapper struct enabling potentials implemented in InteratomicPotentials.jl to be used in Molly
-struct InteratomicPotentialInter{P<:AbstractPotential}
-	potential::P
-    eaf::Function
-    # TODO: should also use the energy cache Jeremiah had, but ignoring that for now
-    # TODO: Default constructor with energy_and_force probably
-end
+using AtomsIO
 
-# Extending Molly.forces with above struct
-function Molly.forces(inter::InteratomicPotentialInter, sys::AbstractSystem, neighbors=nothing; n_threads=Threads.nthreads())
-    eandf = inter.eaf(sys, inter.potential)
-    eandf.f
-end
-
-function molly_params(sys::AtomsBase.AbstractSystem)
-    coords = [SVector{3}(pos) for pos in position(sys)] # need to be SVector for zero() func to work
-    atoms = [Molly.Atom(mass=atm_mass) for atm_mass in atomic_mass(sys)]
-    atoms_data = [AtomData(element=string(atm_symbol)) for atm_symbol in atomic_symbol(sys)]
-    velocities = [SVector{3}(vel) for vel in velocity(sys)] 
-
-    # TODO: Generalize this. Currently very fragile, assumes you read in a cubic box. Otherwise need to use TriclinicBoundary, but some limitations...
-    # Also by default, assumes directions are periodic..., ignoring boundary_conditions
-    bbox = bounding_box(sys)
-    boundary=CubicBoundary(bbox[1][1],bbox[2][2],bbox[3][3]) # FRAGILE! 
-
-    return NamedTuple((atoms=atoms, atoms_data=atoms_data, coords=coords, velocities = velocities, boundary=boundary))
-end 
-
-############################################################
+include("./v2_molly_interpot_utils.jl")
 
 # load system with AtomsIO
 sys = load_system(ExtxyzParser(), "dump_final.xyz")
@@ -45,12 +13,12 @@ rcut = 8.51u"Å"
 species = [:Ar]
 lj_p = InteratomicPotentials.LennardJones(ϵ,σ,rcut,species)
 
-inter_lj = InteratomicPotentialInter(lj_p,InteratomicPotentials.energy_and_force)
+inter_lj = InteratomicPotentialInter(lj_p, u"eV", u"Å")
 general_inters = (inter_lj,)
 
 # Check force with regular sys obtained with AtomsIO
 f_p = Molly.forces(inter_lj, sys)
-f_p = [uconvert.(u"eV/Å", fi) for fi in f_p]
+#f_p = [uconvert.(u"eV/Å", fi) for fi in f_p]
 
 mp = molly_params(sys)
 
@@ -86,4 +54,4 @@ for i in 1:length(f_ref)
 end
 
 @show maximum(fcomp_errs)
-#7.563394355258879e-15 eV Å⁻¹
+#7.577272143066693e-15 eV Å⁻¹
---
+++ molly/interpot_wrapper/v2_molly_interpot_utils.jl
@@ -0,0 +1,65 @@
+#import Molly: forces, potential_energy
+using Molly
+using InteratomicPotentials
+using Unitful, UnitfulAtomic
+#import AtomsBase: AbstractSystem
+using AtomsBase
+
+
+e_dim = dimension(u"eV")
+l_dim = dimension(u"Å")
+
+def_eunit = u"eV"
+def_lunit = u"Å"
+
+nounit_t = typeof(NoUnits)
+
+struct InteratomicPotentialInter{P<:AbstractPotential}
+    potential::P
+    energy_units::Unitful.Unitlike 
+    length_units::Unitful.Unitlike
+
+    # internal constructor, ensuring energy units and length units have correct dimensions
+    ( InteratomicPotentialInter(pot::AbstractPotential, 
+                                eu::Union{nounit_t, Unitful.Units{UE,e_dim,nothing}} = def_eunit, 
+                                lu::Union{nounit_t, Unitful.Units{UL,l_dim,nothing}} = def_lunit) 
+                                where {UE,UL} = new{typeof(pot)}(pot,eu,lu) )
+end
+
+
+function Molly.forces(inter::InteratomicPotentialInter, 
+                      sys::AbstractSystem,
+                      neighbors = nothing;
+                      n_threads = Threads.nthreads())
+
+    forces = InteratomicPotentials.force(sys,inter.potential)
+
+    # initial profiling didn't show huge performance hit from unit conversion 
+    # but! that may be because other parts of IP.jl are very slow, e.g. neighbor list construction
+    if eltype(forces[1]) <: Unitful.Quantity 
+        if inter.energy_units != NoUnits
+            forces = [uconvert.(inter.energy_units/inter.length_units, fi)
+                     for fi in forces]
+        else
+            forces = [ustrip.(fi)
+                     for fi in forces]
+        end
+    end
+    forces
+end
+
+
+function molly_params(sys::AtomsBase.AbstractSystem)
+    coords = [SVector{3}(pos) for pos in position(sys)] # need to be SVector for zero() func to work
+    atoms = [Molly.Atom(mass=atm_mass) for atm_mass in atomic_mass(sys)]
+    atoms_data = [AtomData(element=string(atm_symbol)) for atm_symbol in atomic_symbol(sys)]
+    velocities = [SVector{3}(vel) for vel in velocity(sys)] 
+
+    # TODO: Generalize this. Currently very fragile, assumes you read in a cubic box. Otherwise need to use TriclinicBoundary, but some limitations...
+    # Also by default, assumes directions are periodic..., ignoring boundary_conditions
+    bbox = bounding_box(sys)
+    boundary=CubicBoundary(bbox[1][1],bbox[2][2],bbox[3][3]) # FRAGILE! 
+
+    return NamedTuple((atoms=atoms, atoms_data=atoms_data, coords=coords, velocities = velocities, boundary=boundary))
+end 
+

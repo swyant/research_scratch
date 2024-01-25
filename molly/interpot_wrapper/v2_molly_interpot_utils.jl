e60d246ad512b3aef9540f6d565c4cc56f6d22b0
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2024-01-25 16:19:33 -0500

    checking reference configurations with the Molly wrapper, updates to molly_params, needed alt nonorthog TiAl

--- molly/interpot_wrapper/v2_molly_interpot_utils.jl
+++ molly/interpot_wrapper/v2_molly_interpot_utils.jl
@@ -31,7 +31,7 @@ function Molly.forces(inter::InteratomicPotentialInter,
                       sys::AbstractSystem,
                       neighbors = nothing;
                       n_threads = Threads.nthreads())
-
+    print(sys)
     forces = InteratomicPotentials.force(sys,inter.potential)
 
     # initial profiling didn't show huge performance hit from unit conversion 
@@ -78,10 +78,10 @@ function molly_params(sys::AtomsBase.AbstractSystem)
     atoms_data = [AtomData(element=string(atm_symbol)) for atm_symbol in atomic_symbol(sys)]
     velocities = [SVector{3}(vel) for vel in velocity(sys)] 
 
-    # TODO: Generalize this. Currently very fragile, assumes you read in a cubic box. Otherwise need to use TriclinicBoundary, but some limitations...
-    # Also by default, assumes directions are periodic..., ignoring boundary_conditions
+    # TODO: maybe check if cubic? Also really need to test the Triclinicboundary thing
     bbox = bounding_box(sys)
-    boundary=CubicBoundary(bbox[1][1],bbox[2][2],bbox[3][3]) # FRAGILE! 
+    #boundary=CubicBoundary(bbox[1][1],bbox[2][2],bbox[3][3]) # FRAGILE! 
+    boundary = TriclinicBoundary(bbox...)
 
     return NamedTuple((atoms=atoms, atoms_data=atoms_data, coords=coords, velocities = velocities, boundary=boundary))
 end 

df623a18189ea479e0a1957c7c80b0bd06a67a69
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2024-03-18 14:24:24 -0400

    debugging the compute pod issue, finding a working solution

--- random_tasks/check_lammps_pod_031224/deconstructed/debug_compute_podd.jl
+++ random_tasks/check_lammps_pod_031224/deconstructed/debug_compute_podd.jl
@@ -1,5 +1,6 @@
 using LAMMPS
 
+
 lmp = LMP(["-screen", "none"])
 
 command(lmp,"units          metal")
@@ -38,27 +39,35 @@ command(lmp,"create_atoms 2 single 4.649515029999999882193151279 4.3144750600000
 command(lmp,"create_atoms 2 single 4.207792519999999925062184047 3.475646489999999921849394013 3.429660650000000199355554287")
 command(lmp,"create_atoms 2 single -0.396484029999999987303738180 0.878939310000000029177158467 4.439574949999999908811787463")
 
-command(lmp, "pair_style    zero 10.0")
-command(lmp, "pair_coeff    * *")
+#command(lmp, "pair_style    zero 10.0")
+#command(lmp, "pair_coeff    * *")
+
+command(lmp,"pair_style     pod")
+command(lmp,"""pair_coeff     * * ../sample_6body_hfo2_param.pod ../sample_6body_2elem_coeffs.pod "" "" Hf O""")
+
 
 command(lmp, """compute ld all pod/atom ../sample_6body_hfo2_param.pod "" ""  Hf O""")
 
 
-#atomids = extract_atom(lmp, "id")
-#sort_idxs = sortperm(atomids)
-#pos = extract_atom(lmp,"x")
+atomids = extract_atom(lmp, "id")
+@show atomids
+sort_idxs = sortperm(atomids)
+pos = extract_atom(lmp,"x")
+@show pos'
 #pos = pos[:,sort_idxs]
 #raw_types = extract_atom(lmp,"type")
 #sorted_types = raw_types[sort_idxs,:]
 
-#command(lmp,"pair_style     pod")
-#command(lmp,"""pair_coeff     * * ../sample_6body_hfo2_param.pod ../sample_6body_2elem_coeffs.pod "" "" Hf O""")
 
 command(lmp,"""compute dd all podd/atom ../sample_6body_hfo2_param.pod "" "" Hf O""")
+command(lmp, "fix storedd all store/state 1 c_dd")
+command(lmp, "fix storedd all store/state 1 c_dd[49]")
 
 ### These commands have the same effect as the extract_compute command below, and cause the issue
-#command(lmp,"variable sample_dd5 equal c_dd[5][49]")
-#command(lmp,"""fix ddprint all print 1 "\${sample_dd5}" file sample_julia_dd.txt""")
+command(lmp,"variable sample_dd5 equal c_dd[5][49]")
+command(lmp,"variable vdd atom f_storedd")
+
+command(lmp,"""fix ddprint all print 1 "\${sample_dd5}" file sample_julia_dd.txt""")
 #command(lmp, "thermo 1")
 ##command(lmp, "thermo_style custom step pe c_dd[5][49]")
 #command(lmp, "thermo_style custom step pe v_sample_dd5")
@@ -76,29 +85,29 @@ command(lmp,"run 0")
 #final_dd = [[zeros(total_num_ld) for _ in 1:3] for __ in 1:num_atoms] 
 
 ### The extract_compute command is the issue
-#raw_dd = extract_compute(lmp,"dd", LAMMPS.API.LMP_STYLE_ATOM,LAMMPS.API.LMP_TYPE_ARRAY)
+raw_dd = extract_compute(lmp,"dd", LAMMPS.API.LMP_STYLE_ATOM,LAMMPS.API.LMP_TYPE_ARRAY)'
 #raw_dd = LAMMPS.extract_compute2(lmp,"dd", LAMMPS.API.LMP_STYLE_ATOM,LAMMPS.API.LMP_TYPE_ARRAY)
-#sorted_dd = raw_dd[sort_idxs,:]
+sorted_dd = raw_dd[sort_idxs,:]
 
 
-command(lmp, "delete_atoms group all")
-
-command(lmp,"mass 1 178.486")
-command(lmp,"mass 2 15.999")
-command(lmp, "change_box all x final 0.000000000000000000000000000 5.136475506554315728635629057 y final 0.000000000000000000000000000 5.193413337544470209650171455 z final 0.000000000000000000000000000 5.246159804521276726063661044 xz final -0.883442968274303019882154331 boundary p p p")
-
-command(lmp,"create_atoms 1 single 0.776203818919444787916006590 2.350166829158174763847455324 3.682797980824850103687140290")
-command(lmp,"create_atoms 1 single 3.046909974681736965607115053 4.944150105556711416454618302 4.138358285486625653959436022")
-command(lmp,"create_atoms 1 single 3.476827233049027299216504616 2.843247525261339170299379475 1.563359341695713888853447315")
-command(lmp,"create_atoms 1 single 1.206121357063365095996232412 0.249264269623342554771383561 1.107798839749943642374319097")
-command(lmp,"create_atoms 2 single 1.992388292737735655535402657 3.756099553796239387537525545 0.094680110732730332623319214")
-command(lmp,"create_atoms 2 single 2.415659899710223434965428169 1.232091591716474976436757061 2.535088502166750856758881127")
-command(lmp,"create_atoms 2 single 2.260642995344876116092791563 1.437314984791091765359283272 5.151477127466908001451884047")
-command(lmp,"create_atoms 2 single 1.837371541104662497900790186 3.961322345404707068183824958 2.711068754608851882892395224")
-command(lmp,"create_atoms 2 single -0.040098562735613671459411478 1.710173262166000407447086218 1.839590910655264810458220381")
-command(lmp,"create_atoms 2 single 4.637992808257605403809975542 4.332359369278528937741157279 0.806657226745606470430516310")
-command(lmp,"create_atoms 2 single 4.293129979308621280154056876 3.483241329365950100793725142 3.406566397691777936529433646")
-command(lmp,"create_atoms 2 single -0.384961544867351790344400797 0.861055037948540880066161662 4.439500168075511510323849507")
+#command(lmp, "delete_atoms group all")
+#
+#command(lmp,"mass 1 178.486")
+#command(lmp,"mass 2 15.999")
+#command(lmp, "change_box all x final 0.000000000000000000000000000 5.136475506554315728635629057 y final 0.000000000000000000000000000 5.193413337544470209650171455 z final 0.000000000000000000000000000 5.246159804521276726063661044 xz final -0.883442968274303019882154331 boundary p p p")
+#
+#command(lmp,"create_atoms 1 single 0.776203818919444787916006590 2.350166829158174763847455324 3.682797980824850103687140290")
+#command(lmp,"create_atoms 1 single 3.046909974681736965607115053 4.944150105556711416454618302 4.138358285486625653959436022")
+#command(lmp,"create_atoms 1 single 3.476827233049027299216504616 2.843247525261339170299379475 1.563359341695713888853447315")
+#command(lmp,"create_atoms 1 single 1.206121357063365095996232412 0.249264269623342554771383561 1.107798839749943642374319097")
+#command(lmp,"create_atoms 2 single 1.992388292737735655535402657 3.756099553796239387537525545 0.094680110732730332623319214")
+#command(lmp,"create_atoms 2 single 2.415659899710223434965428169 1.232091591716474976436757061 2.535088502166750856758881127")
+#command(lmp,"create_atoms 2 single 2.260642995344876116092791563 1.437314984791091765359283272 5.151477127466908001451884047")
+#command(lmp,"create_atoms 2 single 1.837371541104662497900790186 3.961322345404707068183824958 2.711068754608851882892395224")
+#command(lmp,"create_atoms 2 single -0.040098562735613671459411478 1.710173262166000407447086218 1.839590910655264810458220381")
+#command(lmp,"create_atoms 2 single 4.637992808257605403809975542 4.332359369278528937741157279 0.806657226745606470430516310")
+#command(lmp,"create_atoms 2 single 4.293129979308621280154056876 3.483241329365950100793725142 3.406566397691777936529433646")
+#command(lmp,"create_atoms 2 single -0.384961544867351790344400797 0.861055037948540880066161662 4.439500168075511510323849507")
 
 #atomids = extract_atom(lmp, "id")
 #sort_idxs = sortperm(atomids)
@@ -107,7 +116,7 @@ command(lmp,"create_atoms 2 single -0.384961544867351790344400797 0.861055037948
 #raw_types = extract_atom(lmp,"type")
 #sorted_types = raw_types[sort_idxs,:]
 
-command(lmp, "run 0")
+#command(lmp, "run 0")  #!!!!
 
 #raw_ld = extract_compute(lmp,"ld", LAMMPS.API.LMP_STYLE_ATOM,LAMMPS.API.LMP_TYPE_ARRAY)'
 #num_pod_types = 2 
@@ -115,7 +124,8 @@ command(lmp, "run 0")
 #num_perelem_ld = size(raw_ld)[2] + 1 # including 1-body terms
 #total_num_ld = num_pod_types*(num_perelem_ld)
 #final_dd = [[zeros(total_num_ld) for _ in 1:3] for __ in 1:num_atoms] 
-raw_dd = extract_compute(lmp,"dd", LAMMPS.API.LMP_STYLE_ATOM,LAMMPS.API.LMP_TYPE_ARRAY)'
+#raw_dd = extract_compute(lmp,"dd", LAMMPS.API.LMP_STYLE_ATOM,LAMMPS.API.LMP_TYPE_ARRAY)' #!!!!!
 #sorted_dd = raw_dd[sort_idxs,:]
 
 #@show sorted_dd[5,49]
+#testvar = LAMMPS.extract_variable(lmp, "vdd")
---
+++ random_tasks/check_lammps_pod_031224/redo_md_test/molly_pod_test.jl
@@ -0,0 +1,55 @@
+using AtomsIO 
+using Molly 
+using Atomistic 
+using InteratomicPotentials
+using GLMakie
+
+spot_check_sys = load_system("./md_test/spot_check_molly80.xyz")
+
+hfo2_sys_raw = load_system("./md_test/sample_monoclinic_HfO2.xyz")
+hfo2_sys = staticAtoms(hfo2_sys_raw)
+molly_hfo2_sys = Molly.System(hfo2_sys, u"eV", u"eV/Å")
+
+lmp_pod1 = LAMMPS_POD("./sample_6body_hfo2_param.pod","./sample_6body_2elem_coeffs.pod", [:Hf,:O])
+hfo2_lbp1 = LBasisPotential(lmp_pod1)
+
+lmp_pod2 = LAMMPS_POD("./sample_6body_hfo2_param.pod","./sample_6body_2elem_coeffs.pod", [:Hf,:O])
+hfo2_lbp2 = LBasisPotential(lmp_pod2)
+
+pod_inter = InteratomicPotentialInter(hfo2_lbp1, u"eV", u"Å")
+general_inters = (pod_inter,)
+
+m_sys = Molly.System(molly_hfo2_sys;
+                    general_inters=general_inters, 
+                    loggers=(force=ForceLogger(typeof(1.0u"eV/Å"), 1),
+                             energy=PotentialEnergyLogger(typeof(1.0u"eV"),1),
+                             coords=CoordinateLogger(1),
+                             vels=VelocityLogger(1))
+                    )
+         
+#simulator = VelocityVerlet(
+#    dt=0.001u"ps",
+#    remove_CM_motion=0,
+#)
+
+#simulator = VelocityVerlet(
+#    dt=0.001u"ps",
+#    coupling=AndersenThermostat(300u"K", 0.1u"ps"),
+#    remove_CM_motion=0,
+#)
+
+random_velocities!(m_sys,200.0u"K")
+simulator = Langevin(
+    dt=0.001u"ps",
+    temperature=200u"K",
+    friction=1.0u"ps^-1",
+    remove_CM_motion=0,
+)
+
+color_map = Dict(:Hf => :grey, :O => :red)
+size_map = Dict(:Hf => 0.5, :O => 0.25)
+colors = [color_map[sym] for sym in atomic_symbol(m_sys)]
+sizes = [size_map[sym] for sym in atomic_symbol(m_sys)]
+
+#simulate!(m_sys, simulator,1000)
+#visualize(m_sys.loggers.coords, m_sys.boundary, "md_test/test.mp4"; color=colors, markersize=sizes)

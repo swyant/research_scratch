e1e414b199263d619ad18fc656d426cb57c1eb86
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2024-09-06 12:17:39 -0400

    repeating tests with original env, but kokkospod branch of lammps

---
+++ ip/lammps_pod/original_tests/original_env_kokkospod/each_config_quick_test_timing/parse_pod_analysis.jl
@@ -0,0 +1,31 @@
+using InteratomicPotentials
+using AtomsIO
+
+include("../../files/parse_pod_analysis_utils.jl")
+
+data_folder= "../../files/quick_test_set"
+
+lmp_pod=LAMMPS_POD("../../files/sample_6body_hfo2_param.pod", [:Hf, :O])
+hfo2_lbp = LBasisPotential(lmp_pod,"../lammps_fit_quick_test_set/ref_quick_test_HfO2_coefficients.pod")
+
+header=[:config, :num_atoms, :volume, :energy, :DFT_energy, :energy_error, :force, :DFT_force, :force_error]
+ddict = read_segmented_files("../lammps_fit_quick_test_set/ref_quick_test_HfO2_training_analysis.pod"; header=header, delim=' ', ignorerepeated=true, skipto=2)
+
+for fname in keys(ddict)
+    @show xyz_fname =joinpath(data_folder, fname)
+    config_set = load_trajectory(xyz_fname)
+    my_energies = [ ]
+    my_fqois = [ ]
+    @time for config in config_set
+        energy = potential_energy(config, hfo2_lbp)
+        push!(my_energies, energy)
+
+        pred_forces = force(config, hfo2_lbp)
+        force_qoi = sqrt(sum(map(x-> x^2,reduce(hcat, pred_forces))))
+        push!(my_fqois,force_qoi)
+    end
+    
+    @show maximum(abs.(my_energies-ddict[fname][!,:energy]))
+    @show maximum(abs.(my_fqois-ddict[fname][!,:force]))
+
+end

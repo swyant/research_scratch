adc7112a6bb30229db286f79c998a7b2afc96599
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2025-04-07 15:38:32 -0400

    generating sample frenkel data

---
+++ conformal_prediction/Hf_frenkel_exploration/data_gen/generation_round1/pristine_300K/gen_dirs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+for i in {1..10}; do
+  mkdir -p "run_$i"
+
+  cp nvt.in "run_$i/"
+
+  sed -i "s|variable run_idx equal 1|variable run_idx equal $i|" "run_$i/nvt.in"
+
+  sed -i "s|variable rng     equal 389218|variable rng     equal $RANDOM|" "run_$i/nvt.in"
+done

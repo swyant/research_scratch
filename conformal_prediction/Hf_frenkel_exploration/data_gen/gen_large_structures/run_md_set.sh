5d8930b433e772f96ce6b488d314423b81961212
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2025-09-03 10:36:10 -0400

    Hadn't actually committed the gen large structures runs on splinter

--- conformal_prediction/Hf_frenkel_exploration/data_gen/gen_large_structures/run_md_set.sh
+++ conformal_prediction/Hf_frenkel_exploration/data_gen/gen_large_structures/run_md_set.sh
@@ -1,11 +1,21 @@
 #!/bin/bash 
 
-for i in {1..2}; do
+for i in {3..5}; do
     cd large_8x_frenkel/run_$i 
     mpirun -np 20 ~/cesmix/dev/cesmix_lammps/build_fixedPOD_030625/lmp -in nvt.in | tee out.txt
     cd ../../large_dilute_frenkel/run_$i
     mpirun -np 20 ~/cesmix/dev/cesmix_lammps/build_fixedPOD_030625/lmp -in nvt.in | tee out.txt
     cd ../../large_pristine/run_$i
+    mpirun -np 20 ~/cesmix/dev/cesmix_lammps/build_fixedPOD_030625/lmp -in nvt.in | tee out.txt
+    cd ../../
+done
+
+for i in {3..5}; do 
+    cd 18x_frenkel/run_$i 
+    mpirun -np 20 ~/cesmix/dev/cesmix_lammps/build_fixedPOD_030625/lmp -in nvt.in | tee out.txt
+    cd ../../18x_dilute_frenkel/run_$i
+    mpirun -np 20 ~/cesmix/dev/cesmix_lammps/build_fixedPOD_030625/lmp -in nvt.in | tee out.txt
+    cd ../../18x_large_pristine/run_$i
     mpirun -np 20 ~/cesmix/dev/cesmix_lammps/build_fixedPOD_030625/lmp -in nvt.in | tee out.txt
     cd ../../
 done
